957f95e47691d084219a16777deee29a7102a7a4
Dan Samama <dan.samama@gmail.com>
2016-07-20 17:59:34 +0300

    Creating the router to the different products of the shop > getting all of the infos

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by itc_user on 7/19/2016.
+ */
+(function(){
+    'use strict'
+    var shopApp = angular.module('shopApp',['ngRoute']);
+
+    shopApp.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'productBox.html',
+                controller: 'shopController'
+            })
+            .when('/:id', {
+                templateUrl: 'fullInfo.html',
+                controller: 'shopController'
+            })
+        .otherwise({redirectTo: '/'});
+    });
+})();
---
+++ directives.js
@@ -0,0 +1,23 @@
+/**
+ * Created by itc_user on 7/19/2016.
+ */
+var shopApp = angular.module('shopApp');
+
+shopApp.directive("navigationBar",function(){
+    return{
+        templateUrl:"topview.html"
+    };
+});
+
+/*
+shopApp.directive("productList",function(){
+    return{
+        templateUrl:"productBox.html"
+    };
+});
+
+shopApp.directive("productDetails",function(){
+    return{
+        templateUrl:"fullInfo.html"
+    };
+});*/
